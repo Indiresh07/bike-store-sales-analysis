72ef45e7eecae0a7c176a6dd0061ed3b4565eeac
Indiresh Sunkara <141450923+Indiresh07@users.noreply.github.com>
2025-08-16 16:51:09 +0530

    Add files via upload

---
+++ queries/top5customers.sql
@@ -0,0 +1,13 @@
+--- this query is used to find top 5 customers purchasing products with their customer_id , name , total_orders , recent purchase date 
+
+
+
+USE my_dataset ;
+SELECT c.customer_id , CONCAT(c.first_name ,'', c.last_name)  AS full_name , COUNT(o.order_id) AS total_orders
+	, MAX(o.order_date) AS last_purchase_date 
+FROM orders AS o
+INNER JOIN customers AS C 
+ON o.customer_id = c.customer_id 
+GROUP BY c.customer_id , c.first_name , c.last_name 
+ORDER BY total_orders DESC , last_purchase_date DESC 
+LIMIT 5 ;
